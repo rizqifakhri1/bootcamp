106ee09fad04ff063743defa8c4d8ffcb4a2ef1a
SE-61\Batch 10 <rizqifakhri@gmail.com>
2024-05-06 14:06:29 +0700

    Latihan Cat

---
+++ ProjectA/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.Tracing;
+
+class Cat 
+{
+    string color;
+    string spesies;
+    public void Eat()
+    {
+        Console.WriteLine("Cat Eat");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Cat Caty = new Cat();
+        Caty.Eat();
+    }
+}
