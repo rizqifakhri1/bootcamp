fb3c052fd42b9aa10d03ad6df3e6aefe6e35bbdf
SE-88\Batch 10 <rizqifakhri@gmail.com>
2024-05-10 14:54:59 +0700

    BelajarParams

---
+++ Day 4/BelajarParams/Program.cs
@@ -0,0 +1,42 @@
+﻿class Calculator
+{
+   public int Add(params int[] number)
+   {
+    int sum = 0;
+    foreach ( int i in number)
+    {
+        sum += i;
+    }
+    return sum;
+   }
+}
+
+class StringClc
+{
+    public int Add(params string[] numberString)
+    {
+        int sum = 0;
+        foreach (string i in numberString)
+        {
+            int number = int.Parse(i);
+            sum += number;
+        }
+
+        return sum;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator clc = new Calculator();
+        int a = clc.Add(1,2,3,4,5); //.Dump
+        Console.WriteLine(a);
+
+        StringClc SClc = new StringClc();
+        int b = SClc.Add("1", "2", "3", "4", "5");
+
+        Console.WriteLine(b);
+    }
+}
