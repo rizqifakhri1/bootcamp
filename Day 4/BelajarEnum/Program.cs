a18d2c8f8a3a20dccdba5bd3a7333dd6fdd69f19
SE-88\Batch 10 <rizqifakhri@gmail.com>
2024-05-10 14:02:41 +0700

    Belajar Enum

---
+++ Day 4/BelajarEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿public enum Hewan 
+{
+    Gajah = 5,
+    Jerapah = 7,
+    Kucing = 0, 
+    KudaLiar,
+    KeptingKikir,
+    CumiDarat,
+    TupaiTerbang = 1 //Harus Unik kalau tidak akan ada dua nilai yang sama
+}
+
+class Program
+{
+    static void Main()
+    {
+        Hewan gajah = Hewan.Gajah;
+        Console.WriteLine(gajah);
+
+        int kudaLiar = (int)Hewan.KudaLiar;
+        Console.WriteLine(kudaLiar);
+
+        int cumiDarat = (int)Hewan.CumiDarat;
+        Console.WriteLine(cumiDarat);
+
+        int tupaiTerbang = (int)Hewan.TupaiTerbang;
+        Console.WriteLine(tupaiTerbang);
+    }
+}
