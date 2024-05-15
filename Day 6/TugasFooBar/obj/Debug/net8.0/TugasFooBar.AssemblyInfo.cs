705ff781c34da852063849586488aaaa07d8f33f
DESKTOP-3JQOKBE\Batch 10 <alif7laksono@gmail.com>
2024-05-15 14:56:42 +0700

    Run Foobar

--- Day 6/TugasFooBar/obj/Debug/net8.0/TugasFooBar.AssemblyInfo.cs
+++ Day 6/TugasFooBar/obj/Debug/net8.0/TugasFooBar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TugasFooBar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b516ef5e525e03eb7600b4f71d81604dc0776ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5d9e7001b3e7aabb572d3badd1230fbd7922eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TugasFooBar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TugasFooBar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
