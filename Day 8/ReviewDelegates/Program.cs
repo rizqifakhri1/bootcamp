e17a678463a27c9acbfc871508a491d953884aba
DESKTOP-3JQOKBE\Batch 10 <alif7laksono@gmail.com>
2024-05-16 14:56:04 +0700

    Latihan dan Review Materi sebelum ujian

---
+++ Day 8/ReviewDelegates/Program.cs
@@ -0,0 +1,26 @@
+﻿public delegate void Calculator(int x, int y);
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator(Hitung.Add);
+        // calculator += Hitung.Add;
+        calculator += Hitung.Sub;
+        calculator.Invoke(3, 2);
+    }
+}
+
+class Hitung 
+{
+    public static void Add(int x, int y)
+    {
+        Console.WriteLine(x + y);
+    }
+
+    public static void Sub(int x, int y)
+    {
+        Console.WriteLine(x - y);
+    }
+}
