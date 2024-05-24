4c766ac4426eb400ff94f0b6f4972a47f681c921
Rizqi <Rizqifakhri@gmail.com>
2024-05-24 14:54:37 +0700

    kasih komen mengenai Conditional Compilation

--- Day 13/ConditionalCompilation/Program.cs
+++ Day 13/ConditionalCompilation/Program.cs
@@ -6,6 +6,8 @@ class Program
     {
         int a = 2;
         int b = 3;
+
+        // dotnet run -c release
         #if DEBUG
             System.Console.WriteLine("Mode Debug");
             System.Console.WriteLine(a+b);
